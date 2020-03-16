a39b31e87b550ce80bf4a844ada69b5bd3cac0df
Sneh <sneha.mishra1793@gmail.com>
2020-03-16 19:30:51 +0530

    commit

--- UnLimit/src/main/java/dao/ProcessFileImpl.java
+++ UnLimit/src/main/java/dao/ProcessFileImpl.java
@@ -1,38 +1,31 @@
 package dao;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import org.apache.poi.ss.util.CellUtil;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.ss.util.CellReference;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellType;
-
 
 public class ProcessFileImpl implements ProcessFile {
-	
-	
+ private int rowCount = 0;
 	@Override
 	public void process(String fileName) throws IOException {
 
@@ -53,35 +46,45 @@ public class ProcessFileImpl implements ProcessFile {
 		}
 
 		// Read Sheet
-
-		HashMap<String, Integer> coloumnNameSheet2 = new HashMap<String, Integer>();
+		File file1 = new File(fileName);
+		FileOutputStream fout = new FileOutputStream(file1);
+		Sheet sheet = addCatalog.createSheet("Updated Sheet");
+		
 		HashMap<String, Integer> coloumnNameSheet1 = new HashMap<String, Integer>();
 
 		Sheet sheet1 = addCatalog.getSheetAt(0);
-		Sheet sheet2 = addCatalog.getSheetAt(1);
-
-		getHeader(coloumnNameSheet1, sheet1, 0);
-		getHeader(coloumnNameSheet2, sheet2, 1);
+		
+		//get header
+		getHeader(coloumnNameSheet1, sheet, sheet1, 0);
+	
 
 		// compare column
-		Map<String, Integer> commonColumn = getCommonColums(coloumnNameSheet1, coloumnNameSheet2);
+		Map<String, Integer> commonColumn = getCommonColums(coloumnNameSheet1);
 		System.out.println("common coloumn");
 		commonColumn.keySet().stream().forEach(col -> System.out.println(col));
 
-		// Get Column Mapping in sheet
-		Map<Integer, Integer> commonColumnMapping = getCommonColumsMapping(commonColumn, coloumnNameSheet2);
-
-		// update sheet
-		updateSheet(fileName, addCatalog, commonColumnMapping, fis);
+		List<Integer> commonColumnIndex = new ArrayList<Integer> (commonColumn.values());
+		Collections.sort(commonColumnIndex);
+		
+		Map<Integer, Integer> commonColumnMapping =new HashMap<Integer, Integer>();
+				
+		for(int i=0;i<commonColumnIndex.size();i++)
+		{
+			// Get Column Mapping in sheet
+			commonColumnMapping.put(commonColumnIndex.get(i), i);
+		}
+		
+		// update sh
+	//	Map<Integer, Integer> commonColumnMapping = getCommonColumsMapping(commonColumn, commonMap);
+		updateSheet(fileName, addCatalog, commonColumnMapping,coloumnNameSheet1, fis,sheet);
 
 	}
 
-// Getting columns names for comparison 
-	private void getHeader(Map<String, Integer> coloumnNameSheet, Sheet sheet, int romNum) {
+	private void getHeader(Map<String, Integer> coloumnNameSheet, Sheet sheet, Sheet sheet1, int romNum) {
 
-		Row row = sheet.getRow(romNum);
+		Row row = sheet1.getRow(romNum);
 
-		System.out.println("Sheet Name is ****" + sheet.getSheetName());
+		System.out.println("Sheet Name is ****" + sheet1.getSheetName());
 
 		for (int i = 0; i < row.getLastCellNum(); i++) {
 			Cell cell = row.getCell(i);
@@ -89,35 +92,26 @@ public class ProcessFileImpl implements ProcessFile {
 			System.out.println("sheet values are" + cell.getColumnIndex() + "" + cell.getStringCellValue());
 			// Add all the cell values of a particular row
 			coloumnNameSheet.put(cell.getStringCellValue(), cell.getColumnIndex());
-
 			CellReference.convertNumToColString(cell.getColumnIndex());
-		}
-	}
+            }
+		
+}
+	
 
-	// getting common columns
-	private Map<String, Integer> getCommonColums(Map<String, Integer> coloumnNameSheet1,
-			Map<String, Integer> coloumnNameSheet2) {
+	private Map<String, Integer> getCommonColums(Map<String, Integer> coloumnNameSheet1) {
+		List<String> commomCol= Arrays.asList(ColumnNameEnum.values()).stream().map(e -> e.getColumnName()).collect(Collectors.toList());
+       
 		Map<String, Integer> commonMap = coloumnNameSheet1.entrySet().stream()
-				.filter(x -> coloumnNameSheet2.containsKey(x.getKey().trim()))
+				.filter(x -> commomCol.contains(x.getKey().trim()))
 				.collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
 
 		return commonMap;
 	}
 
-	private Map<Integer, Integer> getCommonColumsMapping(Map<String, Integer> commonColumnSheet1,
-			Map<String, Integer> coloumnNameSheet2) {
 
-		return commonColumnSheet1.entrySet().stream()
-				.collect(Collectors.toMap(x -> x.getValue(), x -> coloumnNameSheet2.get(x.getKey().trim())));
+	private void updateSheet(String fileName, Workbook addCatalog, Map<Integer, Integer> commonColumsMapping, Map<String, Integer>coloumnNameSheet
+			,FileInputStream fis,Sheet sheet) throws IOException {
 
-	}
-
-	// writing the common data into sheet2
-	private void updateSheet(String fileName, Workbook addCatalog, Map<Integer, Integer> commonColumsMapping,
-			FileInputStream fis) throws IOException {
-
-		// Get second sheet
-		Sheet sheet2 = addCatalog.getSheetAt(1);
 
 		for (Row row : addCatalog.getSheetAt(0)) {
 
@@ -130,20 +124,24 @@ public class ProcessFileImpl implements ProcessFile {
 
 					Row newRow = null;
 					// check row already exist or not
-					if (sheet2.getRow(row.getRowNum() + 2) == null) {
+					if (sheet.getRow(row.getRowNum()) == null) {
 						// create new row
 						// Specific row number
-						newRow = sheet2.createRow(row.getRowNum() + 2);
+						newRow = sheet.createRow(row.getRowNum());
 
 					}
 
 					else {
-						newRow = sheet2.getRow(row.getRowNum() + 2);
+						newRow = sheet.getRow(row.getRowNum());
 					}
 
 					// Specific cell number in sheet 2
 					Cell newCell = newRow.createCell(commonColumsMapping.get(cell.getColumnIndex()));
+	                CellStyle origStyle = cell.getCellStyle();
+	                CellStyle newStyle = addCatalog.createCellStyle();
+	                newStyle.cloneStyleFrom(origStyle);
 
+	                cell.setCellStyle(newStyle);
 					// putting value at specific position
 
 					switch (cell.getCellType()) {
@@ -157,88 +155,85 @@ public class ProcessFileImpl implements ProcessFile {
 				}
 			}
 		}
-
 		File file = new File(fileName);
-		FileOutputStream outputStream = new FileOutputStream(file);
-		addCatalog.write(outputStream);
-	
-		outputStream.close();
+        	FileOutputStream outputStream = new FileOutputStream(file);
+        	addCatalog.write(outputStream);
 
-		System.out.println("File Updated");
-		addHeader(addCatalog, fis, fileName);
-		
-		//FileInputStream fin = new FileInputStream(new File (fileName));
-		//HSSFWorkbook workbook = new HSSFWorkbook(fin);
-		//workbook = new HSSFWorkbook(fin);
-		//Get first sheet from the workbook
-		//HSSFSheet sheet = workbook.getSheetAt(1);
-
-		
-		
+        	System.out.println("File Updated");
+        	outputStream.close();
+        	addHeader(addCatalog, fis, fileName);
 	}
-	private void addHeader(Workbook addCatalog ,FileInputStream fis, String fileName) throws IOException {
-		Cell cell = null;
-		
-		URL urlLoader = ProcessFileImpl.class.getProtectionDomain().getCodeSource().getLocation();
-		String loaderDir = urlLoader.getPath();
-		System.out.printf("loaderDir", loaderDir);
 
-		File file = new File(loaderDir + "/Excel_UnLimit/src/main/resource/UnLimit.xlsx");
+	private void addHeader(Workbook addCatalog, FileInputStream fis, String fileName) throws IOException {
+        Cell cell = null;
 
-		FileInputStream inputStream = new FileInputStream(file);
+        URL urlLoader = ProcessFileImpl.class.getProtectionDomain().getCodeSource().getLocation();
+        String loaderDir = urlLoader.getPath();
+        System.out.println("loaderDir " + loaderDir);
 
-		Workbook refBook = new XSSFWorkbook(inputStream);
-		Sheet sheet = refBook.getSheetAt(0);
-		System.out.println("Sheet Name is ****" + sheet.getSheetName());
-		
-		addCatalog = new HSSFWorkbook(fis);
-	      Sheet sheetManual =   addCatalog.getSheetAt(1);
-	      Row row = sheetManual.getRow(1);
-			// get the last column index
-			int maxcell = row.getLastCellNum();
-			System.out.println(maxcell);
-			cell = row.getCell(maxcell+1);
-			 if(cell == null)
-	             cell = row.createCell(maxcell+1);
-		
-	    Iterator<Row> rowIterator = sheet.iterator();
-	    Cell celldata;
-        while (rowIterator.hasNext()) 
-        {
-            Row row1 = rowIterator.next();
-            //For each row, iterate through all the columns
-            Iterator<Cell> cellIterator = row1.cellIterator();
-            
-            while (cellIterator.hasNext()) 
-            {
-            	 celldata = cellIterator.next();
-                //Check the cell type and format accordingly
-                switch(celldata.getCellType())
-                {
-                case STRING:
-                    System.out.println(celldata.getStringCellValue() );
-                    break;
-                case NUMERIC:
-                    System.out.println(celldata.getNumericCellValue() );
-                    break;
-                case BOOLEAN:
-                    System.out.println(celldata.getBooleanCellValue() );
-                    break;
-                }
+        File file = new File(loaderDir + "/UnLimit.xlsx");
 
-       		 cell.setCellValue(celldata.getStringCellValue());
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook refBook = new XSSFWorkbook(inputStream);
+        Sheet sheetRef = refBook.getSheetAt(0);
+        System.out.println("Sheet Name is ****" + sheetRef.getSheetName());
+
+        Sheet sheetManual = addCatalog.getSheetAt(1);
+
+
+        Iterator<Row> rowIterator = sheetRef.iterator();
+        while (rowIterator.hasNext()) {
+
+            Row rowInputSheet = rowIterator.next();
+
+            Row row = sheetManual.getRow(rowCount);
+            // get the last column index
+            int maxcell = row.getLastCellNum();
+            int maxcellRef = row.getLastCellNum();
+
+            System.out.println(maxcell);
+            // For each row, iterate through all the columns
+            Iterator<Cell> cellIterator = rowInputSheet.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell celldata = cellIterator.next();
+
+                celldata.getStringCellValue();
+                cell = row.getCell(maxcell);
+                if (cell == null)
+                    cell = row.createCell(maxcell);
+                System.out.println(celldata.getStringCellValue());
+                cell.setCellValue(celldata.getStringCellValue());
+
+                //Apply Style
+                CellStyle origStyle = celldata.getCellStyle();
+                CellStyle newStyle = addCatalog.createCellStyle();
+                newStyle.cloneStyleFrom(origStyle);
+
+                cell.setCellStyle(newStyle);
+                maxcell++;
             }
-            System.out.println("");
+            sheetRef.getMergedRegions().forEach(cellAddresses ->
+                    {
+                        if (cellAddresses.getFirstRow() == rowCount) {
+                            sheetManual.addMergedRegion(new CellRangeAddress(cellAddresses.getFirstRow(), cellAddresses.getLastRow(), cellAddresses.getFirstColumn() + maxcellRef, cellAddresses.getLastColumn() + maxcellRef));
+                        }
+                    }
+            );
+
+            rowCount++;
+            System.out.print(" ");
 
         }
-         
-    FileOutputStream out = new FileOutputStream(new File(fileName));
-            addCatalog.write(out);
-		        out.close();		
-		
-		        inputStream.close();
-		addCatalog.close();
-		fis.close();
-	}
+
+
+        FileOutputStream out = new FileOutputStream(fileName);
+        addCatalog.write(out);
+        out.close();
+        addCatalog.close();
+        fis.close();
+        inputStream.close();
+    }
 
 }
